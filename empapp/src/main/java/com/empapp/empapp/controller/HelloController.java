a5e409e7e6336f6c748459e610afecd82ff5a4ee
Tahasildarrashid <tahasildarrashid233@gmail.com>
2025-02-13 20:39:01 +0530

    Initial commit for docker_networking

---
+++ empapp/src/main/java/com/empapp/empapp/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.empapp.empapp.controller;
+
+import com.empapp.empapp.service.InstanceInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private InstanceInformationService instanceInformationService;
+
+    @GetMapping(path="hello-world")
+    public String hello(){
+        return "hello empapp v3  :"+instanceInformationService.retrieveInstanceInfo();
+    }
+}
